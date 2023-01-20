eeb29f3ed7a2234bcb1b92722fd61b64687b3eb2
smithy6 <115367713+smithy6@users.noreply.github.com>
2023-01-20 11:01:20 +0000

    run.cs

---
+++ runmain.cs
@@ -0,0 +1,13 @@
+using System;
+
+class MainProg
+{
+    static void Main(string[] args)
+    {
+        // Create a new instance of the Program class
+        Program scrabbleGame = new Program();
+
+        // Run the game logic
+        scrabbleGame.Run();
+    }
+}
